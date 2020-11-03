0d38221044836ac24cc7a545805e333b45911843
qfdk <qfdk2010@gmail.com>
2020-11-04 00:03:27 +0100

    first commit

---
+++ src/components/server/CreateServer.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+      <el-breadcrumb-item>添加新服务器</el-breadcrumb-item>
+    </el-breadcrumb>
+    <h3>新建服务器</h3>
+  </div>
+</template>
+<script>
+export default {}
+</script>
