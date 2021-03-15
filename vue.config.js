867740552a1371c57b82bb849cd4329c9a6a00b9
qfdk <qfdk2010@gmail.com>
2021-03-15 19:26:13 +0100

    fix: 修改标题显示

--- vue.config.js
+++ vue.config.js
@@ -67,25 +67,14 @@ module.exports = {
       errors: true
     }
   },
-  // configureWebpack: {
-  //   // provide the app's title in webpack's name field, so that
-  //   // it can be accessed in index.html to inject the correct title.
-  //   name: name,
-  //   resolve: {
-  //     alias: {
-  //       '@': resolve('src')
-  //     }
-  //   },
-  //   externals
-  // },
   configureWebpack: config => {
     const plugins = []
     if (isProd) {
       // 为生产环境修改配置...
       // externals里的模块不打包
       Object.assign(config, {
-        externals: externals
-        // name: name,
+        externals: externals,
+        name: name
         // resolve: {
         //   alias: {
         //     '@': resolve('src')
@@ -94,6 +83,9 @@ module.exports = {
       })
     } else {
       // 为开发环境修改配置...
+      Object.assign(config, {
+        name: name
+      })
     }
     config.plugins = [...config.plugins, ...plugins]
   },
@@ -125,14 +117,11 @@ module.exports = {
     config.plugins.delete('prefetch')
 
     // set svg-sprite-loader
-    config.module
-      .rule('svg')
-      .exclude.add(resolve('src/icons'))
-      .end()
-    config.module
-      .rule('icons')
+    config.module.rule('svg').exclude.add(resolve('src/icons')).end()
+    config.module.rule('icons')
       .test(/\.svg$/)
-      .include.add(resolve('src/icons'))
+      .include
+      .add(resolve('src/icons'))
       .end()
       .use('svg-sprite-loader')
       .loader('svg-sprite-loader')
@@ -141,44 +130,42 @@ module.exports = {
       })
       .end()
 
-    config
-      .when(process.env.NODE_ENV !== 'development',
-        config => {
-          config
-            .plugin('ScriptExtHtmlWebpackPlugin')
-            .after('html')
-            .use('script-ext-html-webpack-plugin', [{
-              // `runtime` must same as runtimeChunk name. default is `runtime`
+    config.when(process.env.NODE_ENV !== 'development',
+      config => {
+        config.plugin('ScriptExtHtmlWebpackPlugin')
+          .after('html')
+          .use('script-ext-html-webpack-plugin', [
+            {
+            // `runtime` must same as runtimeChunk name. default is `runtime`
               inline: /runtime\..*\.js$/
             }])
-            .end()
-          config
-            .optimization.splitChunks({
-            chunks: 'all',
-            cacheGroups: {
-              libs: {
-                name: 'chunk-libs',
-                test: /[\\/]node_modules[\\/]/,
-                priority: 10,
-                chunks: 'initial' // only package third parties that are initially dependent
-              },
-              // elementUI: {
-              //   name: 'chunk-elementUI', // split elementUI into a single package
-              //   priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
-              //   test: /[\\/]node_modules[\\/]_?element-ui(.*)/ // in order to adapt to cnpm
-              // },
-              commons: {
-                name: 'chunk-commons',
-                test: resolve('src/components'), // can customize your rules
-                minChunks: 3, //  minimum common number
-                priority: 5,
-                reuseExistingChunk: true
-              }
+          .end()
+        config.optimization.splitChunks({
+          chunks: 'all',
+          cacheGroups: {
+            libs: {
+              name: 'chunk-libs',
+              test: /[\\/]node_modules[\\/]/,
+              priority: 10,
+              chunks: 'initial' // only package third parties that are initially dependent
+            },
+            // elementUI: {
+            //   name: 'chunk-elementUI', // split elementUI into a single package
+            //   priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
+            //   test: /[\\/]node_modules[\\/]_?element-ui(.*)/ // in order to adapt to cnpm
+            // },
+            commons: {
+              name: 'chunk-commons',
+              test: resolve('src/components'), // can customize your rules
+              minChunks: 3, //  minimum common number
+              priority: 5,
+              reuseExistingChunk: true
             }
-          })
-          // https:// webpack.js.org/configuration/optimization/#optimizationruntimechunk
-          config.optimization.runtimeChunk('single')
-        }
-      )
+          }
+        })
+        // https:// webpack.js.org/configuration/optimization/#optimizationruntimechunk
+        config.optimization.runtimeChunk('single')
+      }
+    )
   }
 }
