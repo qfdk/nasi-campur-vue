66e4754555aacbd431ffa1c08d45a97e3b59fd6c
qfdk <qfdk2010@gmail.com>
2021-03-19 22:13:58 +0100

    fix: 手机 CSS 修改

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,41 +1,70 @@
 <template>
-  <div class="login_container">
-    <div class="login_box">
-      <!-- 头部区域 -->
-      <div class="avatar_box">
-        <img alt src="../../assets/img/icon.png">
-      </div>
-      <!-- 表单区域 -->
-      <el-form ref="loginFormRef" :model="loginForm" class="login_form" label-width="0px">
-        <!-- 用户名 -->
-        <el-form-item prop="username">
-          <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user" />
-        </el-form-item>
-
-        <!-- 密码 -->
-        <el-form-item prop="password">
-          <el-input v-model="loginForm.password" :type="passwordType" prefix-icon="iconfont icon-3702mima">
-            <i slot="suffix" class="iconfont icon-showpassword" @click="showPwd" />
-          </el-input>
-        </el-form-item>
-
-        <!-- 按钮 -->
-        <el-form-item class="btns">
-          <el-button type="primary" @click="login">登录</el-button>
-          <el-button type="info" @click="resetLoginForm">重置</el-button>
-        </el-form-item>
-      </el-form>
+  <div class="login-container">
+    <div class="avatar_box">
+      <img alt src="http://qfdk.free.fr/icon.png">
     </div>
+    <el-form
+      ref="loginForm"
+      :model="loginForm"
+      auto-complete="on"
+      class="login-form"
+      label-position="left"
+    >
+      <el-form-item prop="username">
+        <span class="svg-container">
+          <svg-icon icon-class="user" />
+        </span>
+        <el-input
+          ref="username"
+          v-model="loginForm.username"
+          auto-complete="on"
+          name="username"
+          placeholder="Username"
+          tabindex="1"
+          type="text"
+        />
+      </el-form-item>
+
+      <el-form-item prop="password">
+        <span class="svg-container">
+          <svg-icon icon-class="password" />
+        </span>
+        <el-input
+          :key="passwordType"
+          ref="password"
+          v-model="loginForm.password"
+          :type="passwordType"
+          auto-complete="on"
+          name="password"
+          placeholder="Password"
+          tabindex="2"
+          @keyup.enter.native="handleLogin"
+        />
+        <span class="show-pwd" @click="showPwd">
+          <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
+        </span>
+      </el-form-item>
+
+      <el-button
+        :loading="loading"
+        style="width:100%;margin-bottom:30px;"
+        type="primary"
+        @click.native.prevent="handleLogin"
+      >登录
+      </el-button>
+
+    </el-form>
   </div>
 </template>
 
 <script>
+
 export default {
+  name: 'Login',
   data() {
     return {
-      // 登陆表单数据对象
       loginForm: {
-        username: '',
+        username: 'qfdk',
         password: ''
       },
       loading: false,
@@ -58,12 +87,11 @@ export default {
       } else {
         this.passwordType = 'password'
       }
+      this.$nextTick(() => {
+        this.$refs.password.focus()
+      })
     },
-    resetLoginForm() {
-      this.$refs.loginFormRef.resetFields()
-    },
-
-    async login() {
+    async handleLogin() {
       try {
         const res = await this.$http.post('/token/login', {
           password: this.loginForm.password
@@ -77,66 +105,147 @@ export default {
           this.$message.error('登录失败')
         }
       } catch (e) {
-        this.$message.error(e.toString())
+        this.$message.error('登录失败')
       }
     }
   }
 }
 </script>
 
+<style lang="scss" >
+/* 修复input 背景不协调 和光标变色 */
+/* Detail see https://github.com/PanJiaChen/vue-element-admin/pull/927 */
+
+$bg: #283443;
+$light_gray: #fff;
+$cursor: #fff;
+
+@supports (-webkit-mask: none) and (not (cater-color: $cursor)) {
+  .login-container .el-input input {
+    color: $cursor;
+  }
+}
+
+/* reset element-ui css */
+.login-container {
+  .el-input {
+    display: inline-block;
+    height: 47px;
+    width: 85%;
+
+    input {
+      background: transparent;
+      border: 0px;
+      -webkit-appearance: none;
+      border-radius: 0px;
+      padding: 12px 5px 12px 15px;
+      color: $light_gray;
+      height: 47px;
+      caret-color: $cursor;
+
+      &:-webkit-autofill {
+        box-shadow: 0 0 0px 1000px $bg inset !important;
+        -webkit-text-fill-color: $cursor !important;
+      }
+    }
+  }
+
+  .el-form-item {
+    border: 1px solid rgba(255, 255, 255, 0.1);
+    background: rgba(0, 0, 0, 0.1);
+    border-radius: 5px;
+    color: #454545;
+  }
+}
+</style>
+
 <style lang="scss" scoped>
-.login_container {
-  //background: url('../../assets/img/bg.png');
-  height: 100%;
-}
+$bg: #2d3a4b;
+$dark_gray: #889aa4;
+$light_gray: #eee;
 
-.login_box {
-  max-width: 450px;
+.login-container {
+  min-height: 100%;
   width: 100%;
-  height: 300px;
-  background-color: #fff;
-  border-radius: 10px;
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
-}
+  overflow: hidden;
 
-.avatar_box {
-  width: 130px;
-  height: 130px;
-  border-radius: 50%;
-  border: 1px solid #eee;
-  padding: 10px;
-  box-shadow: 0 0 10px #ddd;
-  position: absolute;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  background-color: #fff;
+  .el-form {
+    padding: 20px;
+    border-radius: 10px;
+  }
 
-  img {
-    width: 100%;
-    height: 100%;
+  .avatar_box {
+    width: 130px;
+    height: 130px;
     border-radius: 50%;
-    background-color: #eee;
+    border: 1px solid #eee;
+    padding: 10px;
+    box-shadow: 0 0 10px #ddd;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #fff;
+    margin-top: 170px;
+    z-index: 1;
+
+    img {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      background-color: #eee;
+    }
   }
-}
 
-.login_form {
-  position: absolute;
-  bottom: 0;
-  width: 100%;
-  padding: 0 20px;
-  box-sizing: border-box;
-
-  .icon-showpassword {
-    margin-right: 8px;
-    cursor:pointer;
+  .login-form {
+    position: relative;
+    width: 520px;
+    max-width: 100%;
+    margin: 200px auto;
+    background-color: #000000a1;
+    overflow: hidden;
+    padding: 50px 10px 0px 10px;
   }
-}
 
-.btns {
-  display: flex;
-  justify-content: flex-end;
+  .tips {
+    font-size: 14px;
+    color: #fff;
+    margin-bottom: 10px;
+
+    span {
+      &:first-of-type {
+        margin-right: 16px;
+      }
+    }
+  }
+
+  .svg-container {
+    padding: 6px 5px 6px 15px;
+    color: $dark_gray;
+    vertical-align: middle;
+    width: 30px;
+    display: inline-block;
+  }
+
+  .title-container {
+    position: relative;
+
+    .title {
+      font-size: 26px;
+      color: $light_gray;
+      margin: 0px auto 40px auto;
+      text-align: center;
+      font-weight: bold;
+    }
+  }
+
+  .show-pwd {
+    position: absolute;
+    right: 10px;
+    top: 7px;
+    font-size: 16px;
+    color: $dark_gray;
+    cursor: pointer;
+    user-select: none;
+  }
 }
 </style>
