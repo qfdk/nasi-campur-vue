41f757193e3805599d7fc42ef63548d3807616a5
qfdk <qfdk2010@gmail.com>
2021-05-25 09:05:52 +0200

    fix: 界面微调

--- src/views/users/list.vue
+++ src/views/users/list.vue
@@ -34,7 +34,7 @@
       </el-table-column>
       <el-table-column label="昵称" prop="nickname" min-width="180" />
       <el-table-column v-if="displayMobile" label="国家" prop="server.country" min-width="180" />
-      <el-table-column v-if="displayMobile" label="城市" prop="server.location" min-width="100" />
+      <el-table-column v-if="displayMobile" label="城市" prop="server.location" min-width="180" />
       <el-table-column v-if="displayMobile" label="结束时间" prop="endTime" min-width="100">
         <template slot-scope="scope">
           {{ $dayjs(scope.row.endTime).format('YYYY-MM-DD') }}
